b2754f2b17b955b2389c165a4dd7b020c7d84e1d
Andrey <andrtazet@gmail.com>
2022-01-24 01:18:55 +0300

    req rep server and client

---
+++ client/req_rep.go
@@ -0,0 +1,66 @@
+package client
+
+import (
+	"encoding/json"
+	"errors"
+	"time"
+
+	"github.com/pebbe/zmq4"
+)
+
+type ReqRepClient struct {
+	Client
+	Timeout time.Duration
+}
+
+type ReqRepRequest struct {
+	Endpoint string      `json:"type"`
+	Data     interface{} `json:"data"`
+}
+
+type ReqRepResponse struct {
+	Status bool        `json:"status"`
+	Data   interface{} `json:"data"`
+	Error  string      `json:"error"`
+}
+
+func NewReqRepClient(host, port string, protocol Protocol, context *zmq4.Context, timeout time.Duration) (*ReqRepClient, error) {
+	client, err := NewClient(host, port, protocol, zmq4.REQ, context)
+	if err != nil {
+		return nil, err
+	}
+	if timeout > 0{
+		client.socket.SetRcvtimeo(timeout)
+	}
+	return &ReqRepClient{
+		Client:  *client,
+		Timeout: timeout,
+	}, nil
+}
+
+func (c *ReqRepClient) Request(endpoint string, data interface{}) (*ReqRepResponse, error) {
+	if !c.isConnected {
+		return nil, errors.New("socket is not connected")
+	}
+	message, err := json.Marshal(ReqRepRequest{
+		Endpoint: endpoint,
+		Data:     data,
+	})
+	if err != nil {
+		return nil,err
+	}
+	_, err = c.socket.SendBytes(message, 0)
+	if err != nil {
+		return nil, err
+	}
+	responseData, err := c.socket.RecvBytes(0)
+	if err != nil {
+		return nil, err
+	}
+	response := ReqRepResponse{}
+	err = json.Unmarshal(responseData, &response)
+	if err != nil{
+		return nil, err
+	}
+	return &response, err
+}
---
+++ server/req_rep.go
@@ -0,0 +1,74 @@
+package server
+
+import (
+	"encoding/json"
+	"errors"
+
+	zmq "github.com/pebbe/zmq4"
+)
+
+type ReqRepServer struct{
+	Server
+	handlers map[string]ReqRepServerHandler
+}
+
+type ReqRepResponse struct{
+	Status bool `json:"status"`
+	Data interface{} `json:"data"`
+	Error string `json:"error"`
+}
+
+type reqRepRequest struct{
+	Endpoint string      `json:"type"`
+	Data     interface{} `json:"data"`
+}
+
+type ReqRepServerHandler func(interface{}) ReqRepResponse
+
+
+func NewReqRepClient(host, port string, protocol Protocol, context *zmq.Context) (*ReqRepServer, error){
+	server, err := NewServer(host, port, protocol, zmq.REP, context)
+	if err != nil{
+		return nil, err
+	}
+	return &ReqRepServer{
+		Server:   *server,
+		handlers: map[string]ReqRepServerHandler{},
+	}, nil
+}
+
+func (s *ReqRepServer) NewHandler(endpoint string, handler ReqRepServerHandler) {
+	s.handlers[endpoint] = handler
+}
+
+func (s *ReqRepServer) Start() error {
+	if !s.isBinded{
+		return errors.New("socket is not binded")
+	}
+	go s.Listener()
+	return nil
+}
+
+func (s *ReqRepServer) Listener() {
+	for s.isBinded{
+		data, err := s.socket.RecvBytes(0)
+		if err != nil{
+			panic(err)
+		}
+		request :=  reqRepRequest{}
+		err = json.Unmarshal(data, &request)
+		if err != nil{
+			panic(err)
+		}
+		if handler, ok := s.handlers[request.Endpoint]; ok{
+			resp, err := json.Marshal(handler(request))
+			if err != nil{
+				panic(err)
+			}
+			_, err = s.socket.SendBytes(resp, 0)
+			if err != nil{
+				panic(err)
+			}
+		}
+	}
+}
